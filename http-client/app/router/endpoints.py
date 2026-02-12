0e7e7531315aceb245a4981968f890e09b07f555
Khush Vikram Jain <khush.jain@betsol.com>
2026-02-12 15:01:13 +0530

    Updated the endpoints file and added both valid and invalid

--- http-client/app/router/endpoints.py
+++ http-client/app/router/endpoints.py
@@ -1,4 +1,4 @@
-ENDPOINTS = {
+INVALIDENDPOINTS = {
 "stock_invalid_param_test": {
     "method": "GET",
     "path": "/stock",
@@ -8,3 +8,49 @@ ENDPOINTS = {
 }
 
 }
+
+ENDPOINTS = {
+    "stock_details": {
+        "method": "GET",
+        "path": "/stock",
+        "params": {
+            "name": "Tata Steel"
+        }
+    },
+
+    "industry_search": {
+        "method": "GET",
+        "path": "/industry_search",
+        "params": {
+            "query": "Steel"
+        }
+    },
+
+    "historical_stats_limited": {
+        "method": "GET",
+        "path": "/historical_stats",
+        "params": {
+            "stock_name": "Tata Steel",
+            "stats": "ROE"
+        }
+    },
+
+    "historical_data_limited": {
+        "method": "GET",
+        "path": "/historical_data",
+        "params": {
+            "stock_name": "Tata Steel",
+            "period": "1m",
+            "filter": "price"
+        }
+    },
+
+    "stock_target_price": {
+        "method": "GET",
+        "path": "/stock_target_price",
+        "params": {
+            "stock_id": "TATASTEEL"
+        }
+    }
+}
+
